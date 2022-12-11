27cdf662d6711b9287dce5bb09c110dcf5b9545a
lukaszplk <lpolowski@protonmail.com>
2022-12-11 20:35:08 +0100

    bug fixing

--- Firma/Views/Wszystkie/WszystkieFakturyView.xaml.cs
+++ Firma/Views/Wszystkie/WszystkieFakturyView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Firma.Views.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,7 +19,7 @@ namespace Firma.Views.Wszystkie
     /// <summary>
     /// Logika interakcji dla klasy WszystkieFakturyView.xaml
     /// </summary>
-    public partial class WszystkieFakturyView : UserControl
+    public partial class WszystkieFakturyView : WszystkieViewBase
     {
         public WszystkieFakturyView()
         {
